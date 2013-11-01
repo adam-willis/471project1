583493e453a2f54a86c76ebe8527070f8e0dd814
bigdaddydave <dholmes@cs.odu.edu>
2013-11-01 10:14:14 -0400

    Going back

--- Source.cpp
+++ Source.cpp
@@ -1,43 +1,43 @@
 #include <pthread.h>
 #include <iostream>
 #include <cstdlib>
-#include <conio.h>
-#include <windows.h>
 #include "SaleItem.h"
 
-#define MAX_SIZE 200
-#define BUFF_SIZE 20
+#define MAX_SIZE 20
+#define BUFFER_SIZE 20
 
 using namespace std;
 
-SalesRecord * buff;
+SalesRecord * buff; 
 pthread_t * producerThreads;
 pthread_t * consumerThreads;
 
-int Production_Count;
-int Section_Size;
-int maxProducers=2;
-int maxConsumers=4;
+const int numProducers=4;
+const int numConsumers=4;
+
+//Shared variables
+int Production_Count=0;                //keeps track of overall production
+int Counter=0;                         //the number of "valid" items in the buffer
+double AllStoreTotals[numProducers];   //array the size of number of producers to track store totals
+double AllMonthTotals[12];             //array the size of number of months to track month totals
+int WritePosition;                     //where the producer writes next
+int ReadPosition;                      //where the consumer reads next
 
 //function proto types
-void ProduceConsume(int maxProducers, int maxConsumers);
+void ProduceConsume(int numProducers, int numConsumers);
 void *ProducerFunction(void * producer_data);
 void *ConsumerFunction(void * consumer_data);
 
 struct ProducerData{
 	int ID;
-	int low; //keeps track of low index for producer
-	int high; //keeps track of the high index for producer
-	int writehere; //where the producer writes next
-	int readhere; //where the consumer reads from next
-	int count; //current count of items producer has in its buffer section
 };
 
 struct ConsumerData{
+	int ID;
+	double ConsumerStoreTotals[numProducers];//local statistics
+	double ConsumerMonthTotals[12];//local statistics
 };
 
-ProducerData	* producer_data;
-ConsumerData	* consumer_data;
 
 pthread_mutex_t producer_mutex;
 pthread_mutex_t consumer_mutex;
@@ -46,48 +46,52 @@ pthread_cond_t consumer_condition;
 
 int main(){
 
-	ProduceConsume(maxProducers, maxConsumers);
+	ProduceConsume(numProducers, numConsumers);
 	cout << Production_Count << endl;
 
 	system("pause");
 	return 0;
 }
 
-void ProduceConsume(int maxProducers, int maxConsumers){
+void ProduceConsume(int numProducers, int numConsumers){
 	pthread_mutex_init(&producer_mutex, NULL);
 	pthread_mutex_init(&consumer_mutex, NULL);
 	pthread_cond_init(&producer_condition, NULL);
 	pthread_cond_init(&consumer_condition, NULL);
 
-	Production_Count = 0;
-	buff = new SalesRecord[BUFF_SIZE];
+	
+	ProducerData	* producer_data;  //pointer to the ProducerData Struct
+	ConsumerData	* consumer_data;  //pointer to the ConsumerData Struct
 
-	producerThreads = new pthread_t[maxProducers];
-	consumerThreads = new pthread_t[maxConsumers];
-	producer_data = new ProducerData[maxProducers];
-	consumer_data = new ConsumerData[maxConsumers];
-
-	Section_Size = BUFF_SIZE / maxProducers;
+	producerThreads = new pthread_t[numProducers];  //array to hold the Producer Threads
+	consumerThreads = new pthread_t[numConsumers];  //array to hold the Consumer Threads
+	producer_data = new ProducerData[numProducers]; //array to hold the Producer Structs
+	consumer_data = new ConsumerData[numConsumers]; //array to hold the Consumer Structs
+	
+	buff=new SalesRecord[BUFFER_SIZE];
 
 	//create consumers first so that they are ready to consume when the first producer signals
-	for (int i = 0; i < maxConsumers; i++){		
+	for (int i = 0; i < numConsumers; i++){		
+		//creates the consumer thread "i" which uses consumer struct "i" as variable
+		consumer_data[i].ID=i;
 		pthread_create(&consumerThreads[i], NULL, ConsumerFunction, (void*) &consumer_data[i]);
 	}
 
 	//create producers and set their IDs
-	for (int i = 0; i < maxProducers; i++){
+	for (int i = 0; i < numProducers; i++){
+		//creates the producer thread "i" which uses producer struct "i" as variable
 		producer_data[i].ID = i;
 		pthread_create(&producerThreads[i], NULL, ProducerFunction, (void*) &producer_data[i]);
 	}
 
 	//wait for threads to finish
-	for (int i = 0; i < maxConsumers; i++)
+	for (int i = 0; i < numConsumers; i++)
 		pthread_join(consumerThreads[i], NULL);
-	for (int i = 0; i < maxProducers; i++)
+	for (int i = 0; i < numProducers; i++)
 		pthread_join(producerThreads[i], NULL);
 
 	//output of buffer - Debugging tool
-	for (int i=0; i<BUFF_SIZE; i++){
+	for (int i=0; i<BUFFER_SIZE; i++){
 		cout<<"Index: "<<i<<"  ";
 		buff[i].display();
 	}
@@ -102,41 +106,27 @@ void ProduceConsume(int maxProducers, int maxConsumers){
 }
 
 void *ProducerFunction(void * producer_data){
-	ProducerData * pd = (ProducerData*) producer_data;
+	ProducerData * pd = (ProducerData*) producer_data; //make a pointer point to the correct producer_data struct
+
 	//seed each thread by raising seed by the power of the thread/producer ID
 	srand(time(NULL)^pd->ID);
-	//divide threads along the buffer
-	pd->low = 0 + Section_Size * pd->ID;
-	pd->high = pd->low + Section_Size - 1;
-	if(pd->ID == (maxProducers - 1))
-		pd->high = BUFF_SIZE - 1;
+	
+	while(Production_Count<MAX_SIZE)
+	{
+		while(Counter==BUFFER_SIZE); //Do nothing (no room to write anything) - This can be changed to use mutex instead
 
-pd->count=0;
-	//******************** Changed to track each section's start/stop ******************************
-
-	for(int i = pd->low; (i <= pd->high) && (Production_Count < BUFF_SIZE); i++){
-		
-		pthread_mutex_lock(&producer_mutex); //lock the shared count
-		Production_Count++; //This is the global production count NOT the producers 
-		pthread_mutex_unlock(&producer_mutex); //unlock the shared count
-		
-		//if (Production_Count < MAX_SIZE)  //may use this condition if there is a race condition
-		buff[i] = SalesRecord(pd->ID);
-		pd->count=pd->count++;
-
-				pthread_mutex_lock(&producer_mutex); //lock the shared count
-		cout<<"Producer: "<<pd->ID<<" Current Count"<<pd->count<<endl;
-				pthread_mutex_unlock(&producer_mutex); //unlock the shared count
-
-
-	//*************************************************************************************************
-
+		buff[WritePosition]=SalesRecord(pd->ID);
+		WritePosition=(WritePosition+1)%BUFFER_SIZE;
+				
+		//pthread_mutex_lock(&producer_mutex); //lock the shared count
+		Production_Count++;//This is the global production count
+		Counter++;//This is the count of valid objects in buffer that cannot be overwritten
+		//pthread_mutex_unlock(&producer_mutex); //unlock the shared count
 	}
 
 	pthread_exit(NULL);
 	return 0;
 }
-
 
 void *ConsumerFunction(void * consumer_data){
 	//not done
