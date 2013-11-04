2e26d044c9c0b175c054fbd68c2a343ad8c8780f
Adam Willis <adamwillis83@gmail.com>
2013-11-03 21:47:59 -0500

    changes to dynamic

--- SaleItem.cpp
+++ SaleItem.cpp
@@ -2,7 +2,7 @@
 //default constructor should never be used, but it's there
 SalesRecord::SalesRecord()
 {
-	Day=10;
+	Day=0;
 	Month=0;
 	Year=0;
 	Register=0;
@@ -77,6 +77,7 @@ double SalesRecord::RandomAmount()
 //display
 void SalesRecord::display() const
 {
+	cout<<fixed<<setprecision(2);
 	cout<<"Date: "<<setw(2)<<setfill('0')<<getDay()<<"/"
 		<<setw(2)<<setfill('0')<<getMonth()<<"/"
 		<<setw(2)<<setfill('0')<<getYear()<<"  "
--- Source.cpp
+++ Source.cpp
@@ -1,148 +1,319 @@
-#include <pthread.h>
-#include <iostream>
 #include <cstdlib>
+#include <Windows.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <iostream>
 #include "SaleItem.h"
 
-#define MAX_SIZE 7
-#define BUFF_SIZE 7
+//Function Prototypes
+void ProduceConsume (int, int);
+void *ProducerFunction (void *);
+void *ConsumerFunction (void *);
+void DisplayLocalConsumerData();
+void DisplayGlobalStatistics();
 
-using namespace std;
+//Input Values Arrays
+int numProducersArr[3]  = {2, 5, 10};
+int numConsumersArr[3] = {2, 5, 10};
 
-//arrays to hold buffer, producers, and consumers
-SalesRecord * buff;
-pthread_t * producerThreads;
-pthread_t * consumerThreads;
+//Shared Values
+int NUM_ITEMS=100;					//This can actually stay constant after program is running
+int NUM_PRODUCERS;					//Represents user input, will change to dynamic
+int NUM_CONSUMERS;					//Represents user input, will change to dynamic
+int BSIZE;							//Represents user input, will change to dynamic
+int TotalProduced=0, TotalConsumed=0;		//global values to track the number produced/consumed
+int ReadPosition=0, WritePosition=0;		//the read/right position in the buffer
+double * StoreTotals;//[NUM_PRODUCERS];			//Store-wide total sales (Store held in respective index)
+double MonthTotals[13];						//Month-wide total sales (Month held in respective index)
+SalesRecord * Buffer;//[BSIZE];					//Used to hold StoreItems Produced Until Consumed
 
-//Global variables so the functions need not to pass them
-int Production_Count;
-int Section_Size;
-int numProducers=2;
-int numConsumers=2;
+/*Problems to overcome with making the above values dynamic:
+Buffer size is needed to track location of read/write - can make a SaleItem pointer and update size after user input
+Producer size is needed - Used to create threads(already done dynamically) and consumer must track producer data
+Consumer size is needed - USed to create threads(already done dynamically) and ConsumerData structs needs number */
 
-//called in main
-void ProduceConsume(int maxProdroducers, int numConsumers);
+sem_t MakeItem, TakeItem;					//Semaphore to indicate if buffer is full or has space
+sem_t ProducedMutex, ConsumedMutex;			//Semaphore to ensure mutex on TotalProduced, TotalConsumed
+sem_t ReadMutex, WriteMutex;				//Semaphore to ensure mutex on ReadPosition, WritePosition 
 
-//location and Id data for producer threads
-struct ProducerData{
-	int ID;
-	int low;
-	int high;
+struct ConsumerData
+{
+	int ID;									//consumer thread ID 
+	double ConMonthTotals[13];		//monthly totals for each consumer (index based on month 1-12 used)
+	double * ConStoreTotals;			//store totals for each consumer (index based on producer number)
+};
+ConsumerData * CData;//[NUM_CONSUMERS];			//array to hold ConsumerData structs
+
+//Pointers to arrays for producerThreads, ConsumerThreads
+pthread_t * ProducerThreads;
+pthread_t * ConsumerThreads;
+
+struct runTimeData{
+	int numConsumers;
+	int numProducers;
+	float runTime;
 };
 
-//consumer data, empty
-struct ConsumerData{
-};
+int main()
+{
+	clock_t t1, t2;								//time variables for start time and end time
+	runTimeData runTimes[9];
+	int times = 0;
 
-//pointers to the structs
-ProducerData	* producer_data;
-ConsumerData * consumer_data;
+	cout << "Input size of buffer: ";	cin >> BSIZE;	cout << endl;
 
-//called when we create a thread
-void *ProducerFunction(void * producer_data);
-void *ConsumerFunction(void * consumer_data);
+	Buffer = new SalesRecord[BSIZE];
+	StoreTotals = new double[NUM_PRODUCERS];
+	
+	for(int p = 0; p < 3; p++)
+	{
+		NUM_PRODUCERS = numProducersArr[p];
+		for(int c = 0; c < 3; c++)
+		{
+			t1=clock();							//starting time
+			NUM_CONSUMERS = numConsumersArr[c];
+			
+			ProduceConsume(NUM_PRODUCERS, NUM_CONSUMERS);
 
-//create mutexes and condition variables
-pthread_mutex_t producer_mutex;
-pthread_mutex_t consumer_mutex;
-pthread_cond_t producer_condition;
-pthread_cond_t consumer_condition;
+			t2=clock();							//ending time
+			
+			runTimes[times].numProducers = NUM_PRODUCERS;
+			runTimes[times].numConsumers = NUM_CONSUMERS;
+			runTimes[times].runTime = ((float)t2-(float)t1)/CLOCKS_PER_SEC;;
+			times++;
+			//cout<<"The Total Run Time Was: "<<diff/CLOCKS_PER_SEC<<" Seconds."<<endl;
+		}
+	}
 
-int main(){
-
-
-
-	ProduceConsume(numProducers, numConsumers);
-	cout << Production_Count << endl;
+	for(int p = 0; p < 9; p++)
+	{
+		cout << "The Total Run Time with " << runTimes[p].numProducers;
+		cout << " producers and "				<< runTimes[p].numConsumers;
+		cout << " consumers:\t"				<< runTimes[p].runTime;
+		cout << "\tseconds."						<< endl;
+	}
 
 	system("pause");
+
 	return 0;
 }
 
-void ProduceConsume(int numProducers, int numConsumers){
-	//initialize the mutexes and conditional variables
-	pthread_mutex_init(&producer_mutex, NULL);
-	pthread_mutex_init(&consumer_mutex, NULL);
-	pthread_cond_init(&producer_condition, NULL);
-	pthread_cond_init(&consumer_condition, NULL);
+void ProduceConsume (int NUM_PRODUCERS, int	 NUM_CONSUMERS)
+{
+	int rc1, rc2;
 
-	//Begin tracking how many items are produced
-	Production_Count = 0;
-	//Dynamically allocated array
-	buff = new SalesRecord[BUFF_SIZE];
-	//Producer thread array
-	producerThreads = new pthread_t[numProducers];
-	//Consumer thread array
-	consumerThreads = new pthread_t[numConsumers];
-	//Producer data array
-	producer_data = new ProducerData[numProducers];
-	//Consumer data array, empty data
-	consumer_data = new ConsumerData[numConsumers];
+	//Create the arrays to hold the producers and consumer threads
+	ProducerThreads	=	new pthread_t[NUM_PRODUCERS];
+	ConsumerThreads	=	new pthread_t[NUM_CONSUMERS];
+	CData				=	new ConsumerData[NUM_CONSUMERS];
+	//CData->ConStoreTotals = new double[NUM_PRODUCERS];
 
-	//determine section of array assigned to each thread
-	Section_Size = BUFF_SIZE / numProducers;
+	//initialize the semaphores 
+	//parameters for sem_init(pointer to the semaphore, level of sharing, initial value)
+	sem_init(&MakeItem, 0, BSIZE);
+	sem_init(&TakeItem, 0, 0);
+	sem_init(&ReadMutex, 0, 1);
+	sem_init(&WriteMutex, 0, 1);
+	sem_init(&ProducedMutex, 0, 1);
+	sem_init(&ConsumedMutex, 0, 1);
 
-	//create consumers first so that they are ready to consume when the first producer signals
-	for (int i = 0; i < numConsumers; i++){
-		pthread_create(&consumerThreads[i], NULL, ConsumerFunction, (void*) &consumer_data[i]);
+	//create the consumers
+	for (int t=0; t<NUM_CONSUMERS; t++)
+	{
+		cout<<"Creating consumer thread: "<<t<<endl;
+		CData[t].ID=t;
+		CData[t].ConStoreTotals = new double[NUM_PRODUCERS];
+		rc1=pthread_create(&ConsumerThreads[t], NULL, ConsumerFunction, (void*) &CData[t]);
+		if (rc1)
+		{
+			cout<<"Error, return code from pthread_create is: "<<rc1<<endl;
+			exit (-1); //return codes if threads fail to create
+		}
 	}
-	//create producers and set their IDs
-	for (int i = 0; i < numProducers; i++){
-		producer_data[i].ID = i;
-		pthread_create(&producerThreads[i], NULL, ProducerFunction, (void*) &producer_data[i]);
+
+	//create the producers
+	for (int t=0; t<NUM_PRODUCERS; t++)
+	{
+		cout<<"Creating producer thread: "<<t<<endl;
+		rc2=pthread_create(&ProducerThreads[t], NULL, ProducerFunction, (void*) t);
+		if (rc2)
+		{
+			cout<<"Error, return code from pthread_create is: "<<rc2<<endl;
+			exit (-1);
+		}
 	}
 
 	//wait for threads to finish
-	for (int i = 0; i < numConsumers; i++)
-		pthread_join(consumerThreads[i], NULL);
-	for (int i = 0; i < numProducers; i++)
-		pthread_join(producerThreads[i], NULL);
+	for (int i = 0; i < NUM_CONSUMERS; i++)
+		pthread_join(ConsumerThreads[i], NULL);
+	for (int i = 0; i < NUM_PRODUCERS; i++)
+		pthread_join(ProducerThreads[i], NULL);
 
 	//print the buffer for debugging
-	for(int i = 0; i < BUFF_SIZE; i++){
-		buff[i].display();
+	cout<<"The current buffer (last "<<BSIZE<<" produced):"<<endl;
+	for(int i = 0; i < BSIZE; i++)
+	{
+		Buffer[i].display();
 		cout <<endl;
 	}
 
+	cout<<"Total Produced: "<<TotalProduced<<endl;
+
+	//Display the Local Consumer Data
+	DisplayLocalConsumerData();
+
+	//Display the Global Statistics
+	DisplayGlobalStatistics();
 
 
-	//destroy arrays
-	delete[] producer_data;
-	delete[] consumer_data;
+	//perform cleanup of any dynamically allocated items
+	sem_destroy(&MakeItem);
+	sem_destroy(&TakeItem);
+	sem_destroy(&ReadMutex);
+	sem_destroy(&WriteMutex);
+	sem_destroy(&ProducedMutex);
+	sem_destroy(&ConsumedMutex);
 
-	//destroy mutexes and condition variables
-	pthread_mutex_destroy(&producer_mutex);
-	pthread_mutex_destroy(&consumer_mutex);
-	pthread_cond_destroy(&producer_condition);
-	pthread_cond_destroy(&consumer_condition);
 }
 
-void *ProducerFunction(void * producer_data){
-	srand(1020);
+void *ProducerFunction (void *t)
+{
+	int ProducerID=(long)t;
+	cout<<"Producer: "<<ProducerID<<" started"<<endl;
+	//pick a new random seed for each thread
+	srand((int)time(NULL)^(int)ProducerID);
 
-	//pointer to producer data
-	ProducerData * pd = (ProducerData*) producer_data;
+	//infinate loop until treads complete
+	while(1)
+	{
+		sem_wait(&ProducedMutex);			//All producers wait while checking the number produced
+		if (TotalProduced==NUM_ITEMS)		//Check NUM_ITEMS has been produced yet
+		{
+			sem_post(&ProducedMutex);		//Remove wait after answer is known
+			cout<<"Producer: "<<ProducerID<<" done"<<endl;
+			pthread_exit(NULL);					//Exit thread if true
+		}
+		TotalProduced++;							//Increment since an item will be produced when thread has opportunity
+		sem_post(&ProducedMutex);			//Remove wait after TotalProduced is updated
 
-	//divide threads along the buffer
-	pd->low = Section_Size * pd->ID;
-	pd->high = pd->low + Section_Size - 1;
-	if(pd->ID == numProducers - 1)
-		pd->high = BUFF_SIZE - 1;
-	
-	for(int i = pd->low; i <= pd->high && Production_Count <= MAX_SIZE; ++i){
-		pthread_mutex_lock(&producer_mutex); //lock the shared count
-		Production_Count++;
-		pthread_mutex_unlock(&producer_mutex); //unlock the shared count
-		//if (Production_Count < MAX_SIZE)  //may use this condition if there is a race condition
-		buff[i]= SalesRecord(pd->ID);
+		sem_wait(&MakeItem);					//Do Nothing - wait for signal from consumers to make an item
+
+		//No longer waiting - there is now free buffer space
+		sem_wait(&WriteMutex);				//Other producers wait while the buffer is written to and WritePostion is changed
+		Buffer[WritePosition]=SalesRecord(ProducerID);
+		WritePosition=(WritePosition+1)%BSIZE;
+		sem_post(&WriteMutex);				//Remove wait on buffer and WritePosition
+		sem_post(&TakeItem);				//Signal waiting consumers it's ok to take an item
+		Sleep(int(rand()%36)+5);			//randomly sleep for 5-40 milliseconds
 	}
-
-	pthread_exit(NULL);
 	return 0;
 }
 
+void * ConsumerFunction (void *t)
+{
+	//int ConsumerID=(long)t;
+	ConsumerData * consumer = (ConsumerData*) t;
 
-void *ConsumerFunction(void * consumer_data){
-	//not done
-	pthread_exit(NULL);
+	cout<<"Consumer: "<<consumer->ID<<" started"<<endl;
+
+	//infinate loop until threads complete
+	while(1)
+	{
+		sem_wait(&ConsumedMutex);			//All consumers wait while checking the number consumed
+		if(TotalConsumed==NUM_ITEMS)		
+		{
+			sem_post(&ConsumedMutex);		//Remove wait after answer is known
+			cout<<"Consumer: "<<consumer->ID<<" done"<<endl;
+			pthread_exit(NULL);				//Exit thread if true
+		}
+		TotalConsumed++;					//Increment since an item will be consumed when thread has opportunity
+		sem_post(&ConsumedMutex);			//Remove wait after TotalConsumed is updated
+this_thread:sleep:(10);
+		sem_wait(&TakeItem);				//Do Nothing - wait for signal from producer that an item is available
+
+		//No longer waiting on item to be made
+		sem_wait(&ReadMutex);				//Other consumers wait while the item is being read and ReadPosition is changed
+		//CData[ConsumerID].ID=ConsumerID;
+		CData[consumer->ID].ConMonthTotals[Buffer[ReadPosition].getMonth()]	+= Buffer[ReadPosition].getSaleAmount();
+		CData[consumer->ID].ConStoreTotals[Buffer[ReadPosition].getStoreID()]	+= Buffer[ReadPosition].getSaleAmount();
+		ReadPosition=(ReadPosition+1)%BSIZE;
+		sem_post(&ReadMutex);				//Remove wait on buffer and ReadPosition
+		sem_post(&MakeItem);				//Signal Producers that it's ok to make an item
+	}	
+
 	return 0;
+}
+
+void DisplayLocalConsumerData()
+{
+	for (int i=0; i<NUM_CONSUMERS; i++)
+	{
+		cout<<"========================================================"<<endl;
+		cout<<"Local Consumer Data Report - Consumer: "<<CData[i].ID<<endl;
+		cout<<"========================================================"<<endl;
+		cout<<"Monthly Totals:"<<endl;
+		cout<<"========================================================"<<endl;
+		cout<<"Jan = $"<<CData[i].ConMonthTotals[1]<<endl;
+		cout<<"Feb = $"<<CData[i].ConMonthTotals[2]<<endl;
+		cout<<"Mar = $"<<CData[i].ConMonthTotals[3]<<endl;
+		cout<<"Apr = $"<<CData[i].ConMonthTotals[4]<<endl;
+		cout<<"May = $"<<CData[i].ConMonthTotals[5]<<endl;
+		cout<<"Jun = $"<<CData[i].ConMonthTotals[6]<<endl;
+		cout<<"Jul = $"<<CData[i].ConMonthTotals[7]<<endl;
+		cout<<"Aug = $"<<CData[i].ConMonthTotals[8]<<endl;
+		cout<<"Sep = $"<<CData[i].ConMonthTotals[9]<<endl;
+		cout<<"Oct = $"<<CData[i].ConMonthTotals[10]<<endl;
+		cout<<"Nov = $"<<CData[i].ConMonthTotals[11]<<endl;
+		cout<<"Dec = $"<<CData[i].ConMonthTotals[12]<<endl<<endl;
+		cout<<"========================================================"<<endl;
+		cout<<"Store Totals:"<<endl;
+		cout<<"========================================================"<<endl;
+		for (int j=0; j<NUM_PRODUCERS; j++)
+		{
+			cout<<"Store "<<j<<" = $"<<CData[i].ConStoreTotals[j]<<endl;
+		}
+		cout<<endl<<endl;
+	}
+}
+
+void DisplayGlobalStatistics()
+{
+	//store totals
+	for (int i=0; i<NUM_PRODUCERS; i++)
+	{
+		for (int j=0; j<NUM_CONSUMERS; j++)
+		{		
+			StoreTotals[i]+=CData[j].ConStoreTotals[i];	
+		}
+	}
+	cout<<"========================================================"<<endl;
+	cout<<"Global Store Statistics: "<<endl;
+	cout<<"========================================================"<<endl;
+	for (int i=0; i<NUM_PRODUCERS; i++)
+	{
+		cout<<"Store "<<i<<" = $"<<StoreTotals[i]<<endl;
+	}
+	//month Totals
+	for (int i=0; i<13; i++)
+	{
+		for (int j=0; j<NUM_CONSUMERS; j++)
+		{
+			MonthTotals[i]+=CData[j].ConMonthTotals[i];
+		}
+	}
+	cout<<"========================================================"<<endl;
+	cout<<"Global Monthly Statistics: "<<endl;
+	cout<<"========================================================"<<endl;
+	cout<<"Jan = $"<<MonthTotals[1]<<endl;
+	cout<<"Feb = $"<<MonthTotals[2]<<endl;
+	cout<<"Mar = $"<<MonthTotals[3]<<endl;
+	cout<<"Apr = $"<<MonthTotals[4]<<endl;
+	cout<<"May = $"<<MonthTotals[5]<<endl;
+	cout<<"Jun = $"<<MonthTotals[6]<<endl;
+	cout<<"Jul = $"<<MonthTotals[7]<<endl;
+	cout<<"Aug = $"<<MonthTotals[8]<<endl;
+	cout<<"Sep = $"<<MonthTotals[9]<<endl;
+	cout<<"Oct = $"<<MonthTotals[10]<<endl;
+	cout<<"Nov = $"<<MonthTotals[11]<<endl;
+	cout<<"Dec = $"<<MonthTotals[12]<<endl<<endl;
 }
